c75a614989bebd00a16ae9dd838e524b90eedb6d
Dan Payne <dpayne5532@gmail.com>
2022-11-15 08:21:56 -0700

    thru reduce

--- main.js
+++ main.js
@@ -69,6 +69,78 @@ var eComp = companies.filter(comp => comp.start >= 1980 && comp.start < 1990);
 
 // MAP ====================================================================
 
+// create array of company names
+
+var companyNames = companies.map(function(company) {
+  return company.name;
+});
+
+// console.log(companyNames);
+
+var youth = ages.map(function(age) {
+  return age - 5;
+});
+
+// console.log(youth);
+
+
+// SORT =======================================================================
+
+
+// this will sort companies by start date
+
+var sorted = companies.sort(function(a, b) {
+  if (a.start > b.start) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+// console.log(sorted)
+
+var sortComp = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+
+// console.log(sortComp === sorted);
+
+// sort ages
+
+var sortAges = ages.sort((a, b) => a - b);
+
+//console.log(sortAges);
+
+// [5, 12, 13, 15, 16, 20, 21, 25, 32, 33, 44, 45, 54, 61, 64]
+
+
+//  reduce
+
+var sum = ages.reduce(function(total, age) {
+  return total + age;
+}, 0);
+// console.log(sum);
+
+var ageSum = ages.reduce((total, age) => total + age, 0);
+// console.log(ageSum);
+
+
+var totalYears = companies.reduce(function(total, company) {
+  return total + (company.end - company.start);
+}, 0);
+
+// console.log(totalYears);
+
+var totYears = companies.reduce((total, company) => total + (company.end - company.start), 0);
+
+
+// console.log(totYears);
+
+
+var combined = ages
+  .map(age => age * 2) // Multiply Ages by 2
+  .filter(age => age >= 40) // filter out ages under 40
+  .reduce((a, b) => a + b, 0);  // add all of the ages together
+  console.log(combined);  // 798
+
 
 
 
